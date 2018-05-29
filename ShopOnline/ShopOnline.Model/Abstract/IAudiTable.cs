b4b0633c75782b40b9c1082f240e73a85a573ec6
truonghoang81 <truongviethoang30111996@gmail.com>
2018-05-30 00:49:23 +0700

    add them vao Model

---
+++ ShopOnline/ShopOnline.Model/Abstract/IAudiTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShopOnline.Model.Models
+{
+    public interface IAudiTable
+    {
+        DateTime? CreateDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdateBy { get; set; }
+        bool Status { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+    }
+}
